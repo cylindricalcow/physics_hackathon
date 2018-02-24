40b1318d69b4b91f0e692205c365a73a40a80957
Jonathan Richman <cylindricalcow@gmail.com>
2018-02-24 15:57:37 -0600

    stable dns test

--- simpleserv.py
+++ simpleserv.py
@@ -1,3 +1,9 @@
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+import random 
+from test_func import *
 
 # Copyright (c) Twisted Matrix Laboratories.
 # See LICENSE for details.
@@ -12,15 +18,17 @@ class Echo(protocol.Protocol):
     def dataReceived(self, data):
         "As soon as any data is received, write it back."
         self.transport.write(data)
-
+        iter_test(1,3,100,0)
 
 def main():
     """This runs the protocol on port 8000"""
     factory = protocol.ServerFactory()
     factory.protocol = Echo
     reactor.listenTCP(8000,factory)
+    #print("Test!")
+    
     reactor.run()
-
+    
 # this only runs if the module was *not* imported
 if __name__ == '__main__':
     main()
