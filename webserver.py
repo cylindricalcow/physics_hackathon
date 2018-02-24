332ac30cde5737b4b57f91cb0a13153086b6d318
PartiicularlyPythonicBS <anilr2@illinois.edu>
2018-02-24 11:50:38 -0600

    exploring the http web server system

--- webserver.py
+++ webserver.py
@@ -4,8 +4,8 @@ from twisted.internet import reactor
 class Simple(resource.Resource):
     isLeaf = True
     def render_GET(self, request):
-        return "<html>this actually works!</html>"
+        return "<html>Damn this actually works (http website as of now)!</html>"
 
 site = server.Site(Simple())
 reactor.listenTCP(8080, site)
-reactor.run()
+reactor.run()		  
