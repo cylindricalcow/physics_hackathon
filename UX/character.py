4177f8e4b3222eda113022aa393fb40c64cce0e0
Orlando Melchor-Alonso <mlchrln2@illinois.edu>
2018-02-25 02:12:59 -0600

    UI Status Changes

--- UX/character.py
+++ UX/character.py
@@ -6,6 +6,11 @@ from kivy.uix.widget import Widget
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.image import Image
 from kivy.graphics import Line
+from kivy.properties import ObjectProperty
+from kivy.uix.popup import Popup
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.label import Label
 
 Builder.load_string("""
 <MainScreen>:
@@ -34,13 +39,28 @@ Builder.load_string("""
             on_press: 
                 root.manager.current = 'third'
                 root.manager.transition.direction = 'right'
-        Spinner:
-            text: "Yay!"
+        RelativeLayout:
+            orientation: "horizontal"
+            pos: (root.width*.4, root.height*.15)
+            size_hint: .2,.1
+            # When clicked the popup opens
+            Button:
+                text: "Stats"
+                on_press: root.open_popup()
+    GridLayout:
+        Label:
+            text: "Health Points: 10"
             size_hint: .15,.05
-            pos: (root.width/4, root.height/4)
-            values: ["Why?", "Fuck.", "Shit."]
-            id: spinner_id
-            on_text: root.spinner_clicked(spinner_id.text)
+            pos: (root.width*0.85/3, root.height/4)
+        Label:
+            text: "Armor Class: 10"
+            size_hint: .15,.05
+            pos: (root.width*0.85/2, root.height/4)
+        Label:
+            text: "Speed: 10"
+            size_hint: .15,.05
+            pos: (root.width*.85, root.height/4)
+
 
 <SecondScreen>:
     RelativeLayout:
@@ -61,7 +81,40 @@ Builder.load_string("""
             on_press: 
                 root.manager.current = 'main'
                 root.manager.transition.direction = 'left'
+
+<CustomPopup>:
+    title: "Stats"
+    size_hint: .5, .5
+    auto_dismiss: False
+    GridLayout:
+        cols: 3
+        rows: 3
+        pos: root.x, root.height
+        Label:
+            text: "STR: 10"
+        Label:
+            text: "DEX: 10"
+        Label:
+            text: "CON: 10"
+        Label:
+            text: "INT: 10"
+        Label:
+            text: "WIS: 10"
+        Label:
+            text: "CHA: 10"
+        Label:
+            size_hint:.2,.2
+        Button:
+            size_hint:.2,.2
+            text: "Close"
+            on_press: root.dismiss()
+        Label:
+            size_hint:.2,.2
 """)
+
+class CustomPopup(Popup):
+    pass
+ 
 
 class Painter(Widget):
     
@@ -76,6 +129,10 @@ class MainScreen(Screen):
     #toggle drop down menu
     def spinner_clicked(self, value):
         print("Spinner Value " + value)
+        # Opens Popup when called
+    def open_popup(self):
+        the_popup = CustomPopup()
+        the_popup.open()
 
 class SecondScreen(Screen):
 	pass
