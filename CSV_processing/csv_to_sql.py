b3c6237ac9d9d7989d9a2f51c7f6bce2fbb76f09
Jonathan Richman <cylindricalcow@gmail.com>
2018-02-25 00:10:09 -0600

    not real

--- CSV_processing/csv_to_sql.py
+++ CSV_processing/csv_to_sql.py
@@ -7,4 +7,4 @@ path="D://physics_hackathon//"
 db_path=path+"Databases//"
 string = "Spells"
 df=pd.read_csv(path+"CSV//Databases - "+string+".csv")
-df.to_sql(string, CONNECTION STRING) #Figure out how to make connection string
+df.to_sql(string, db_path+string+".db") #Figure out how to make connection string
