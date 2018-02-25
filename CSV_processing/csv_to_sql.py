9882c60ee863e61cdcf7cd5e83c90d2f15d532b0
Grace Sun <gsun8@illinois.edu>
2018-02-25 06:35:29 -0600

    finally in database

--- CSV_processing/csv_to_sql.py
+++ CSV_processing/csv_to_sql.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import sqlite3
 
-path = "C:/Users/Grace Sun/physics_hackathon/CSV" 
-db_path = path+'/Databases'
+path = "C:/Users/Grace Sun/physics_hackathon/" 
+db_path = path+'/Databases/'
 strings = ["Attacks", "Characters", "Cleric", "Conditions", "Damage Types", "Fighter", "Items", "Monsters", "Rogue", "Save Types", "Spells", "Weapons", "Wizard"]
 
 for string in strings:
 	conn = sqlite3.connect(db_path+string+".db")
-	df = pd.read_csv(path+"/Databases - "+string+".csv")
+	df = pd.read_csv(path+"CSV/Databases - "+string+".csv")
 	df.to_sql(string, conn, if_exists = 'replace')
-	conn.close 
+	conn.close() 
