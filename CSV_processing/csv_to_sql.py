590349dcbe823e781dfbde41dbd5f0ef84e0af8a
Grace Sun <gsun8@illinois.edu>
2018-02-25 00:53:50 -0600

    sqlite3 instead of sqlalchemy + connection string added

--- CSV_processing/csv_to_sql.py
+++ CSV_processing/csv_to_sql.py
@@ -1,10 +1,9 @@
 import pandas as pd
-import glob
-import sys
-import os
+import sqlite3 as lite
 
-path="D://physics_hackathon//" 
-db_path=path+"Databases//"
-string = "Spells"
-df=pd.read_csv(path+"CSV//Databases - "+string+".csv")
-df.to_sql(string, CONNECTION STRING) #Figure out how to make connection string
+path = "C:/Users/Grace Sun/physics_hackathon/CSV" 
+db_path = path+'/Databases'
+string = "Attacks"
+conn = lite.connect(db_path)
+df = pd.read_csv(path+"/Databases - "+string+".csv")
+df.to_sql(string, conn) #Figure out how to make connection string
