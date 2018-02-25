00856c295917fbeb1557054d28a2e9bad853a810
Jonathan Richman <cylindricalcow@gmail.com>
2018-02-24 20:11:21 -0600

    Started code for making databases. Need to figure out how to make connection strings for SQLAlchemy

---
+++ CSV_processing/csv_to_sql.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import glob
+import sys
+import os
+
+path="D://physics_hackathon//" 
+db_path=path+"Databases//"
+string = "Spells"
+df=pd.read_csv(path+"CSV//Databases - "+string+".csv")
+df.to_sql(string, CONNECTION STRING) #Figure out how to make connection string
