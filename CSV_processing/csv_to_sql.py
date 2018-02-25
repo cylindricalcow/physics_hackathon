66b10067b31357f02c3e1cd993b25e577faaecbd
Grace Sun <gsun8@illinois.edu>
2018-02-25 01:39:41 -0600

    csv to sql for all databases in CSV folder as a for loop

--- CSV_processing/csv_to_sql.py
+++ CSV_processing/csv_to_sql.py
@@ -3,7 +3,9 @@ import sqlite3 as lite
 
 path = "C:/Users/Grace Sun/physics_hackathon/CSV" 
 db_path = path+'/Databases'
-string = "Attacks"
+strings = ["Attacks", "Characters", "Conditions", "Damage Types", "Monsters", "Save Types", "Spells", "Weapons"]
 conn = lite.connect(db_path)
-df = pd.read_csv(path+"/Databases - "+string+".csv")
-df.to_sql(string, conn) #Figure out how to make connection string
+
+for string in strings:
+	df = pd.read_csv(path+"/Databases - "+string+".csv")
+	df.to_sql(string, conn, if_exists = 'replace') 
