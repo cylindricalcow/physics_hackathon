e2b5b3a5d10d02a58468b208134ebef740e7a460
Jonathan Richman <cylindricalcow@gmail.com>
2018-02-27 02:13:27 -0600

    fixed some bugs

--- 0th Order/personal_grid.py
+++ 0th Order/personal_grid.py
@@ -2,20 +2,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
+#These are test classes for the server that will act like the UI. The goal is to get this
+#class to work with the engine and server. The grid class 
+
 class Grid:
-    def __init__(self,x,y,N):    
-        self.x=x
-        self.y=y
-        self.N=N  #N must be odd
+    #A class for what a person would see on their screen
+    def __init__(N):    
+        self.N=N  #N is gridsize aka vision radius
 
     def create_local_grid(self):
-        return np.meshgrid(np.arange(floor(self.N/2),np.arangefloor(self.N/2)))
+        return np.meshgrid(np.arange(-1*floor(self.N/2),floor(self.N/2)),floor(self.N/2),floor(self.N/2)))
         
-    
-    def arrow_to_help(self, team_coords, monster_coords):
+    def plot_map(self, team_coords, monster_coords):
     #Use server to find teammates positions in the future                                     
     #team_coords is dict of 2 element arrays
     #monster_coords is 2 element array
+        grid_x,grid_y=self.create_local_grid()
         x_friends=[]
         y_friends=[]                                 
         for key in team_coords.keys():
@@ -23,10 +25,16 @@ class Grid:
             y_friends.append(team_coords[key][1])
         plt.scatter(x_friends,y_friends, color='blue')
         plt.scatter(monster_coords[0],monster_coords[1], color='red')
-       # plt.xlim(np.arange(-math.floor(self.N/2)),math.floor(self.N/2))
-       # plt.ylim(np.arange(-math.floor(self.N/2)),math.floor(self.N/2))                                 
+        plt.scatter([0],[0], color='green')
+        plt.xlim(grid_x)
+        plt.ylim(grid_y)                                 
         plt.grid(True)
         plt.show()
 
-test=Grid(0,0,10)
-test.arrow_to_help({'Bob':[2,4], 'Rob':[3,2], 'Bobert':[0,5]},[2,2])
+    def arrow_endpoint(self, target_x, target_y):
+         #Want to make arrow for characters outside of vision. Don't sure if this is a
+         #good feature, but it could be useful
+         pass
+         
+test=Grid(10)
+test.arrow_to_help({'Alice':[11,7],'Bob':[2,4], 'Rob':[3,2], 'Bobert':[0,5]},[2,2])
